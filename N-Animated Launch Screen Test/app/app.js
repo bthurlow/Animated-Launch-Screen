403e13e4d742f65cbed5560da5df7bc32cdeebb5
Brian Thurlow <bthurlow0424@gmail.com>
2016-03-08 22:13:53 +0000

    Initial Commit

--- N-Animated Launch Screen Test/app/app.js
+++ N-Animated Launch Screen Test/app/app.js
@@ -1,8 +1,6 @@
 var application = require('application');
 
-application.mainModule = 'navigation/navigation';
-// START_CUSTOM_CODE_nativeScriptApp
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+//application.mainModule = 'navigation/navigation';
+application.mainModule = "/pages/launch/launch";
 
-// END_CUSTOM_CODE_nativeScriptApp
 application.start();
